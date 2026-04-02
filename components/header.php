e8690b90318ac42a6a49be3caf304fea0ffa5776
asepewa123 <asepewa123@gmail.com>
2026-04-02 11:14:24 +0200

    feat:Footer icons
    footer icons allemaal links gegeven naar de correcte social media websites

--- components/header.php
+++ components/header.php
@@ -1,19 +1,6 @@
 <!DOCTYPE html>
 <html lang="nl">
 
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="description" content="Ontdek de authentieke smaken van het Midden-Oosten bij Saffraan & Sahara in Den Haag. Geniet van traditionele gerechten, een warme sfeer en uitstekende service. Reserveer nu!" />
-  <meta name="keywords" content="Midden-Oosters restaurant, Den Haag, authentieke gerechten, Saffraan & Sahara, menu, reserveren" />
-  <meta name="author" content="Ehsaan Ahmad en Asepewa Adegbaju" />
-  <title>Saffraan &amp; Sahara - Den Haag</title>
-  <link rel="stylesheet" href="style/style.css" />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;600;700&family=Lato:wght@300;400;700&family=Playfair+Display:ital,wght@0,700;1,400&display=swap" rel="stylesheet" />
-</head>
-
 <header class="site-header">
 
     <figure class="logo">
